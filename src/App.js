0bffac2fc66ef7f701c58608cb13390224c16a26
Saitore973 <saitorevg@gmail.com>
2022-05-30 09:46:05 +0300

    add a hero tag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="container">
       
-      
+      <h1>Weather App</h1>
 
        <Weather />
 
--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ export default function Weather(){
             <form>
                 <input type="text" />
                 <input type="submit" value="Search" />
-            </form>
+            </form> <br /> <br />
             <div className="row">
           <div className="col-md-2">
           <img className="img-responsive" src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="current "/>
